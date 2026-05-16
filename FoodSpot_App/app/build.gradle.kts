9404b4a1478f0c53eeeae5fb4f4e9b092199a572
giselarivas2112 <00016124@uca.edu.sv>
2026-05-16 17:19:54 -0600

    feat: Agregar dependencias para navigation 3

--- FoodSpot_App/app/build.gradle.kts
+++ FoodSpot_App/app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.kotlin.serialization)
+
 }
 
 android {
@@ -55,4 +57,7 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+    implementation(libs.androidx.navigation3.runtime)
+    implementation(libs.androidx.navigation3.ui)
+    implementation(libs.kotlinx.serialization.core)
 }
